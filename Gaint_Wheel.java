9cb02ae5cc68377d83f40f98154a5e30851149c5
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2024-04-17 21:25:57 +0430

    Add files via upload
    https://www.codechef.com/problems/GIANT
    Gaint_Wheel problem form 17 Apr 2024 Wed 19:00 contest code in Java

---
+++ Gaint_Wheel.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Gaint_Wheel
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scan = new Scanner (System.in);
+		int x = scan.nextInt();
+		if (x>=60)
+		System.out.println("Yes");
+		else
+		System.out.println("No");
+
+	}
+}
